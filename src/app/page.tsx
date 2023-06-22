e1add1a956ebb138ac9e552f5e4cdd08288f65a5
DevilsDomain <alaraerzincan@gmail.com>
2023-06-22 17:25:41 +0200

    created a TodoItem Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import TodoItem from '@/components/TodoItem'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      <p>hello world</p>
+      <h1>TODO LIST</h1>
+      <TodoItem />
     </main>
   )
 }
